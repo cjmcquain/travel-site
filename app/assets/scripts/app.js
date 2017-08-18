0da63e84f516aa8980b316af1f9ffe403c95c721
Charles McQuain <charlesmcquain@gmail.com>
2017-08-17 22:27:18 -0700

    Added webpack

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,10 @@
+var Person = require('./modules/Person');
+
+console.log(Person.exampleProperty);
+/*
+var john = new Person("John Doe", "Blue");
+john.greet();
+
+var jane = new Person("Jane Smith", "Red");
+jane.greet(); 
+*/
